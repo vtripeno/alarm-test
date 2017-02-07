2d98b9678d70159c54249fb4a0a60d48ac62d179
Victor Tripeno <victor.tripeno@gft.com>
2017-02-07 17:15:02 -0200

    integração de verificação de chamada juntamente com banco de dados (broadcastreceiver)

--- TestAlarm/app/src/main/java/com/treinamento/victortripeno/testalarm/receiver/AlarmBroadcast.java
+++ TestAlarm/app/src/main/java/com/treinamento/victortripeno/testalarm/receiver/AlarmBroadcast.java
@@ -1,6 +1,5 @@
 package com.treinamento.victortripeno.testalarm.receiver;
 
-import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -11,40 +10,41 @@ import android.widget.Toast;
 import com.treinamento.victortripeno.testalarm.dao.AlarmDAO;
 import com.treinamento.victortripeno.testalarm.modelo.Alarme;
 
+import java.text.ParseException;
+import java.util.Calendar;
+
 /**
  * Created by victor.tripeno on 03/02/2017.
  */
 public class AlarmBroadcast extends BroadcastReceiver {
-    AlarmManager alarmManager;
     PendingIntent pendingIntent;
     AlarmDAO dao;
     Alarme alm;
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        /*alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         Calendar calendar = Calendar.getInstance();
         Intent myIntent = new Intent(context, AlarmReceiver.class);
         pendingIntent = PendingIntent.getBroadcast(context, 0, myIntent, 0);
-        alarmManager.set(AlarmManager.RTC, calendar.getTimeInMillis(), pendingIntent);
         alm = new Alarme();
+        dao = new AlarmDAO(context);
+        boolean flgAlm = false;
         try {
             for (Alarme alarme : dao.buscarAlarmes()) {
-                if(alarme.getTempo().getTime().getHours() == 3) {
+                if(alarme.getTempo().getTime().getHours() == calendar.getTime().getHours() &&
+                        alarme.getTempo().getTime().getMinutes() == calendar.getTime().getMinutes()) {
                     alm = alarme;
+                    flgAlm = true;
                     break;
                 }
             }
         } catch (ParseException e) {
             e.printStackTrace();
         }
-        long timeInMillis = 2000;
-        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, alm.getTempo().getTimeInMillis(), timeInMillis, pendingIntent);
-        alarmManager.notifyAll();*/
-        Toast.makeText(context, "TESTE", Toast.LENGTH_SHORT).show();
-        Log.d("TESTE Intent", "TESTE Intent Broadcast");
-        /*AlarmBroadcast alarmBroadcast = new AlarmBroadcast();
-        IntentFilter intentFilter = new IntentFilter(intent.ACTION_TIME_TICK);*/
+        if(flgAlm) {
+            Toast.makeText(context, "TESTE " + alm.toString(), Toast.LENGTH_LONG).show();
+            Log.d("TESTE Intent", "TESTE Intent Broadcast");
+        }
 
     }
 
